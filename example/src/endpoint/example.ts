1daa32c4402f88e571258ece4526577b33ac1cec
Justin Kaseman <justinkaseman@live.com>
2021-03-01 15:55:45 -0500

    More best practices in the example EA

--- example/src/endpoint/example.ts
+++ example/src/endpoint/example.ts
@@ -1,14 +1,14 @@
 import { Requester, Validator } from '@chainlink/external-adapter'
 import { ExecuteWithConfig, Config } from '@chainlink/types'
 
-export const NAME = 'example'
+export const NAME = 'example' // This should be filled in with a lowercase name corresponding to the API endpoint
 
 const customError = (data: any) => data.Response === 'Error'
 
 const customParams = {
   base: ['base', 'from', 'coin'],
   quote: ['quote', 'to', 'market'],
-  path: false,
+  field: false,
 }
 
 export const execute: ExecuteWithConfig<Config> = async (request, config) => {
@@ -18,18 +18,19 @@ export const execute: ExecuteWithConfig<Config> = async (request, config) => {
   const jobRunID = validator.validated.id
   const base = validator.validated.data.base
   const quote = validator.validated.data.quote
-  const path = validator.validated.data.path || 'price'
+  const field = validator.validated.data.field || 'price'
   const url = `price`
 
   const params = {
     base,
     quote,
+    api_key: config.apiKey,
   }
 
   const options = { ...config.api, params, url }
 
   const response = await Requester.request(options, customError)
-  const result = Requester.validateResultNumber(response.data, [path])
+  const result = Requester.validateResultNumber(response.data, [field])
 
   return Requester.success(jobRunID, {
     data: config.verbose ? { ...response.data, result } : { result },
